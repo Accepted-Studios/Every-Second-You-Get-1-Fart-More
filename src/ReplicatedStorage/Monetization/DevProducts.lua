cd8848b3058a6278986e8a29a32312895aaea5ad
Adi3210 <efeadesal@gmail.com>
2023-02-21 09:15:49 -0500

    Added DevProducts and Fixed ParticleEvenr
    Hello future me or sis, so far what I did:
    
    1. Made UI and scripted all devproducts to prompt once clicked.
    
    2. Made a server that performs all the functions once devprodcut transaction is done.
    3. Added Nuke Functionality.
    
    4. Fixed Issue with ParticleEvent Preventing Emitter Deleting From Player.

---
+++ src/ReplicatedStorage/Monetization/DevProducts.lua
@@ -0,0 +1,57 @@
+---**Compare Words Function To Check If They Are the Same**---
+local function CompareWords(Name1, Name2)
+	local FilteredName1 = string.lower(Name1:gsub("%s+", "")) --Remove Spaces and make lowerspace
+	local FilteredName2 = string.lower(Name2:gsub("%s+", "")) --Remove Spaces and make lowerspace
+	if FilteredName1 == FilteredName2 then --If Names Are The Same
+		return true --Return True
+	else
+		return false --Return False
+	end
+end
+
+------*Module------
+local DevProducts = {}
+DevProducts.__index = DevProducts
+
+DevProducts.ProductsTable = {
+
+	------Wins Table-----
+	{ Name = "10 Wins", Value = 10, ProductId = "1", Type = "Wins" },
+	{ Name = "100 Wins", Value = 100, ProductId = "123456789", Type = "Wins" },
+	{ Name = "500 Wins", Value = 500, ProductId = "123456789", Type = "Wins" },
+	{ Name = "1000 Wins", Value = 1000, ProductId = "123456789", Type = "Wins" },
+
+	-----FartPower Table-----
+	{ Name = "100 FartPower", Value = 100, ProductId = "2", Type = "FartPower" },
+	{ Name = "1000 FartPower", Value = 1000, ProductId = "123456789", Type = "FartPower" },
+	{ Name = "5000 FartPower", Value = 5000, ProductId = "123456789", Type = "FartPower" },
+
+	----+ 1 FartPower Every Seconds Table------
+	{ Name = "1 FartIncrease", Value = 1, ProductId = "3", Type = "FartIncrease" },
+	{ Name = "3 FartIncrease", Value = 3, ProductId = "123456789", Type = "FartIncrease" },
+	{ Name = "6 FartIncrease", Value = 6, ProductId = "123456789", Type = "FartIncrease" },
+	{ Name = "10 FartIncrease", Value = 10, ProductId = "123456789", Type = "FartIncrease" },
+
+	----Nuke Table-----
+	{ Name = "Nuke", ProductId = "4", Type = "Nuke" },
+}
+
+-----**Function To Get ProductId by Name-----
+function DevProducts:GetProductId(ProductName)
+	for i, Product in pairs(self.ProductsTable) do --- Loop through ProductsTable
+		if CompareWords(Product.Name, ProductName) then --- If ProductName is the same as Product.Name
+			return Product.ProductId --- Return ProductId
+		end
+	end
+end
+
+-----**Function To Get ProductInfo by ProductId-----
+function DevProducts:GetProductInfoTableByProductId(ProductId)
+	for i, Product in pairs(self.ProductsTable) do --- Loop through ProductsTable
+		if Product.ProductId == ProductId then --- If ProductId is the same as Product.ProductId
+			return Product --- Return Product
+		end
+	end
+end
+
+return DevProducts
