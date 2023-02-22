4b8b6acbe2bda13fb4a1d13ebb6750ce7478001d
Adi3210 <efeadesal@gmail.com>
2023-02-21 23:40:19 -0500

    Finished Gamepass Scripting and Added Currency UI
    I have completed the gamepass scripting, all needed now is ID Replacing, I have also made the Currency UI as well as made a script that updates it depending on the players currency.

--- src/ServerStorage/GameModules/Transactions.lua
+++ src/ServerStorage/GameModules/Transactions.lua
@@ -1,4 +1,12 @@
------This is a funcion that can convert a percentage parameter to a number-----
+-----Services-----
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local MarketplaceService = game:GetService("MarketplaceService")
+
+-----Variables-----
+local MonetisationFolder = ReplicatedStorage:WaitForChild("Monetization")
+local GamepassesModule = require(MonetisationFolder.Gamepasses)
+
+----This is a funcion that can convert a percentage parameter to a number-----
 local function ConvertPercentageToNumber(Percentage)
 	local Number = Percentage / 100
 	return Number
@@ -7,6 +15,7 @@ end
 ----Modules----
 local Transactions = {}
 
+----**Transaction for Rebirth**----
 function Transactions.Rebirth(player, Price)
 	local leaderstats = player:WaitForChild("leaderstats")
 	local FartPower = leaderstats:WaitForChild("FartPower")
@@ -19,5 +28,27 @@ function Transactions.Rebirth(player, Price)
 		FartRebirths.Value = FartRebirths.Value + ConvertPercentageToNumber(Price) -- Rebirths are 25% more powerful
 	end
 end
+
+-------**Function to Detect if player has bought a gamepass and update stats accordingly**------
+MarketplaceService.PromptGamePassPurchaseFinished:Connect(function(player, gamepassId, purchased)
+	if purchased then
+		for i, GamepassValue in pairs(GamepassesModule.GamepassesTable) do
+			if GamepassValue.GamepassId == gamepassId then
+				if not GamepassesModule:CheckIfPlayerOwnsGamepass(player, GamepassValue.Name) then --Check if player owns gamepass
+					local leaderstats = player:WaitForChild("leaderstats")
+					if GamepassValue.Type == "x2 FartPower" then
+						local FartPower = leaderstats:WaitForChild("FartPower")
+						FartPower.Value = FartPower.Value * 1 + 2
+					else
+						if GamepassValue.Type == "x2 Wins" then
+							local Wins = leaderstats:WaitForChild("Wins")
+							Wins.Value = Wins.Value * 1 + 2
+						end
+					end
+				end
+			end
+		end
+	end
+end)
 
 return Transactions
--- src/StarterGui/PlayerGuiScripts/ButtonUIScripts/ButtonUIHandler.client.lua
+++ src/StarterGui/PlayerGuiScripts/ButtonUIScripts/ButtonUIHandler.client.lua
@@ -1,27 +1,63 @@
 ---------Services-------
 local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local MarketplaceService = game:GetService("MarketplaceService")
 
 ---------Variables-------
 local Player = Players.LocalPlayer
+local MonetisationFolder = ReplicatedStorage:WaitForChild("Monetization")
+local GamepassesModule = require(MonetisationFolder.Gamepasses)
+
 local PlayerGui = Player:WaitForChild("PlayerGui")
 local ButtonsGui = PlayerGui:WaitForChild("ButtonsGui")
 local ButtonFrame = ButtonsGui:WaitForChild("ButtonFrame")
 local RebirthButton = ButtonFrame:WaitForChild("Rebirth")
 local TeleportButton = ButtonFrame:WaitForChild("Teleport")
+local X2WinsButton = ButtonFrame:WaitForChild("X2 Wins")
+local X2FartPowerButton = ButtonFrame:WaitForChild("X2 Fart Power")
+local PetsButton = ButtonFrame:WaitForChild("Pets")
 
----------Functions-------
----Open Rebirth Gui---
+---------Functions----------
+---**Open Rebirth Gui---
 RebirthButton.MouseButton1Click:Connect(function()
 	--Open Rebirth Gui
 	local RebirthGui = PlayerGui:WaitForChild("RebirthGui")
 	RebirthGui.Enabled = true
 end)
 
----Open Teleport Gui---
+---**Open Teleport Gui---
 TeleportButton.MouseButton1Click:Connect(function()
 	--Open Teleport Gui
 	local TeleportGui = PlayerGui:WaitForChild("TeleportGui")
 	TeleportGui.Enabled = true
+end)
+
+----**Prompt Player To Buy X2 Wins----
+X2WinsButton.MouseButton1Click:Connect(function()
+	local GamepassId = GamepassesModule:GetGamepassId("x2 Wins")
+	if not GamepassId then
+		warn("GamepassId not found for gamepass: " .. X2WinsButton.Name)
+		return
+	end
+	MarketplaceService:PromptGamePassPurchase(Player, GamepassId)
+end)
+
+----**Prompt Player To Buy X2 Fart Power----
+X2FartPowerButton.MouseButton1Click:Connect(function()
+	local GamepassId = GamepassesModule:GetGamepassId(X2FartPowerButton.Name)
+	if not GamepassId then
+		warn("GamepassId not found for gamepass: " .. X2FartPowerButton.Name)
+		return
+	end
+	MarketplaceService:PromptGamePassPurchase(Player, GamepassId)
+end)
+
+----**Open Pets Gui----
+PetsButton.MouseButton1Click:Connect(function()
+	local PetMainGui = PlayerGui:WaitForChild("PetMain")
+	local PetInventory = PetMainGui:WaitForChild("PetInventory")
+
+	PetInventory.Visible = not PetInventory.Visible
 end)
 
 ------**TODO: Write a more faster/efficent/easier to Open Gui system**------
